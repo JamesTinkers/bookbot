2abc8786491d47fa6febbf589f269771ced692ad
JamesTinkers <james.d.miron64@gmail.com>
2024-04-12 21:45:29 -0500

    added word counter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+def opener(filepath):
+    with open(filepath) as book:
+        return book.read()
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-        
+    bookpath= "books/frankenstein.txt"
+    book_contents = opener(bookpath)
+    print(book_contents)
+    print(count_words(book_contents))
+def count_words(content):
+    words = content.split()
+    return len(words)
 main()
