2c67f86ca96d4192b56f0b69a0c59c7f2ee3f5f0
JamesTinkers <james.d.miron64@gmail.com>
2024-04-13 22:35:33 -0500

    added a letter counter

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ def main():
     bookpath= "books/frankenstein.txt"
     book_contents = opener(bookpath)
     print(book_contents)
-    print(count_words(book_contents))
+    print("Here is the word count:", count_words(book_contents))
+    print(count_letters(book_contents))
+
 def count_words(content):
     words = content.split()
     return len(words)
+def count_letters(content):
+    lowered_content = content.lower()
+    letter_count_dict = {}
+    for letter in lowered_content:
+        if letter in letter_count_dict:
+            letter_count_dict[letter] += 1
+        else:
+            letter_count_dict[letter] = 1
+    return letter_count_dict
 main()
